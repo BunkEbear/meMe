295c6e5b86f0755f72b03f811ac2d23a957a92da
Elijah <ehorland@pcs-nyc.org>
2025-10-07 14:59:15 -0400

    pHysics

--- pythonSpace/inputs/numPad.py
+++ pythonSpace/inputs/numPad.py
@@ -60,10 +60,10 @@ class numPadIn(inputSuperclass):
 
                 #if the column hears the row then we know
                 if(GPIO.input(self.columns[i]) == 1):
+                    self.btnPress = [i,n]
 
-                        self.btnPress = [i,n]
-
-                GPIO.output(self.rows[n], GPIO.LOW)
+            
+            GPIO.output(self.rows[n], GPIO.LOW)
             
         return self.btnPress
         
