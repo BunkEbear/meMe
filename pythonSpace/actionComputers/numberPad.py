edba0b2e85324c1d6d2973e12f8adbaa980d9f16
Elijah <ehorland@pcs-nyc.org>
2025-10-28 01:53:24 -0400

    cv2Pisserton

--- pythonSpace/actionComputers/numberPad.py
+++ pythonSpace/actionComputers/numberPad.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from godComplexActionSeperators.actionComputerSC import numPadFace
+from actionComputers.godComplexActionSeperators.actionComputerSC import numPadFace
 
 import subprocess
 
