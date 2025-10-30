91248cd8c3d870b860ea86d69281192284c9a170
Elijah <ehorland@pcs-nyc.org>
2025-10-30 03:19:28 -0400

    mesage

--- pythonSpace/actionComputers/godComplexActionSeperators/superClassMessageExtend.py
+++ pythonSpace/actionComputers/godComplexActionSeperators/superClassMessageExtend.py
@@ -24,10 +24,12 @@ class t9(actionComputers.godComplexActionSeperators.actionComputerSC.numPadFace)
         #not entirely shure why we pass it in but sure
         def addCharacter(self,take,charList):
 
-            print(self.take)
+            #print(self.take)
             
             if take == 0:
                  self.message += charList[take]
+
+                 self.take += 1
 
                  print('take0')
 
