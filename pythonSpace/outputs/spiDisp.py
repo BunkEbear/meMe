6b22559c041387acbfbb7cccd5d9c8881ddda6ad
Elijah <ehorland@pcs-nyc.org>
2025-10-30 14:09:19 -0400

    morning

--- pythonSpace/outputs/spiDisp.py
+++ pythonSpace/outputs/spiDisp.py
@@ -61,13 +61,19 @@ class oledDisp:
             cv.putText(slate, bodyChunksAllOverTheFloor[0], (2, 20), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
 
         if len(bodyChunksAllOverTheFloor) >= 2:
-            cv.putText(slate, bodyChunksAllOverTheFloor[1], (2, 26), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
+            cv.putText(slate, bodyChunksAllOverTheFloor[1], (2, 27), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
 
         if len(bodyChunksAllOverTheFloor) >= 3:
-            cv.putText(slate, bodyChunksAllOverTheFloor[2], (2, 32), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
+            cv.putText(slate, bodyChunksAllOverTheFloor[2], (2, 34), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
 
         if len(bodyChunksAllOverTheFloor) >= 4:
-            cv.putText(slate, bobodyChunksAllOverTheFloordy[3], (2, 38), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
+            cv.putText(slate, bobodyChunksAllOverTheFloord[3], (2, 41), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
+        
+        if len(bodyChunksAllOverTheFloor) >= 5:
+            cv.putText(slate, bobodyChunksAllOverTheFloord[4], (2, 48), cv.FONT_HERSHEY_PLAIN, 0.6, 255, 1)
+
+
+
 
 
         self.displayImage(slate)
